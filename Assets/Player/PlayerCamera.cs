10e2aa3b1e107fc3cf52ed376d7b989d2985f33b
Evan Purl <evan@purlcomputers.com>
2021-03-18 10:16:26 -0600

    First Commit

---
+++ Assets/Player/PlayerCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerCamera : NetworkBehaviour
+{
+    public GameObject CameraMountPoint;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            Transform cameraTransform = Camera.main.gameObject.transform;  //Find main camera which is part of the scene instead of the prefab
+            cameraTransform.parent = CameraMountPoint.transform;  //Make the camera a child of the mount point
+            cameraTransform.position = CameraMountPoint.transform.position;  //Set position/rotation same as the mount point
+            cameraTransform.rotation = CameraMountPoint.transform.rotation;
+        }
+    }
+}
